d0403405aa660946af582acf4f51a172401b965a
kshired <shiroed1211@gmail.com>
2024-06-02 03:26:37 +0900

    chore : add maven publish settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("jvm")
-    `maven-publish`
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 val projectGroup: String by project
@@ -25,4 +27,44 @@ tasks.test {
 
 kotlin {
     jvmToolchain(11)
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = projectGroup,
+        artifactId = "kotlin-json-dsl",
+        version = applicationVersion
+    )
+
+    pom {
+        name.set("Kotlin Json DSL")
+        description.set("Kotlin DSL library that makes it easy to generate json")
+        inceptionYear.set("2024")
+        url.set("https://github.com/kshired/kotlin-json-dsl")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("kshired")
+                name.set("kshired")
+                email.set("shiroed1211@gmail.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/kshired/kotlin-json-dsl.git")
+            developerConnection.set("scm:git:ssh://github.com/kshired/kotlin-json-dsl.git")
+            url.set("https://github.com/kshired/kotlin-json-dsl")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
 }
